49754b2cfc0b0a49c54563abe4337e6ced062dec
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-04-25 16:21:51 -0400

    feat: 🎸 integrate app

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import env from 'dotenv';
+env.config();
+
+import { CONSTANTS } from './constants';
+import { emailTemplate } from './templates/email-template';
+import { SendEmailService } from './services/send-email';
+
+const { NODEMAILER } = CONSTANTS;
+const { AUTH } = NODEMAILER;
+const { USER } = AUTH;
+
+const emailOptions = {
+  from: `Tiago Santos <${USER}>`,
+  to: 'tiagosan040@gmail.com',
+  subject: 'Teste nodemailer',
+  html: emailTemplate,
+};
+
+(async () => {
+  const sendEmail = new SendEmailService(emailOptions);
+
+  await sendEmail.execute();
+})();
